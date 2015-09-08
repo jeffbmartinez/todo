ec174dd591795eece5252db1b251927f814598d9
jeff martinez <jeffbmartinez@gmail.com>
2015-09-07 23:20:42 -0700

    consolidate responses file

--- handler/responses.go
+++ handler/responses.go
@@ -7,6 +7,20 @@ import (
 	"github.com/jeffbmartinez/log"
 )
 
+// BasicResponse creates a handler which responds with a standard response
+// code and message string.
+func BasicResponse(code int) func(response http.ResponseWriter, request *http.Request) {
+	return func(response http.ResponseWriter, request *http.Request) {
+		response.WriteHeader(code)
+		response.Write([]byte(http.StatusText(code)))
+	}
+}
+
+// WriteBasicResponse responds the the request with a BasicResponse
+func WriteBasicResponse(code int, response http.ResponseWriter) {
+	BasicResponse(code)(response, nil)
+}
+
 /*
 WriteJSONResponse writes a json response (with correct http header).
 */
